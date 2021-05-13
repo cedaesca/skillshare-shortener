4ae9e045288cad351bcb4bd7fd16043ec778997f
César Escudero <cedaesca@gmail.com>
2021-05-13 12:53:10 -0400

    feat: basic express configuration

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
+import * as express from 'express';
+import * as helmet from 'helmet';
 
 createConnection()
   .then(async (connection) => {
-    //
+    const app = express();
+    const port = process.env.PORT || 3000;
+
+    // Before middlewares
+    app.use(helmet());
+
+    app.listen(port, () =>
+      console.log(`Server up and running at port ${port}`)
+    );
   })
   .catch((error) => console.error(error));
