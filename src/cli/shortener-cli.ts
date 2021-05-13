4e88430a7e1a46a518e28377322f125e57492b89
César Escudero <cedaesca@gmail.com>
2021-05-13 19:50:47 -0400

    feat: cli added

---
+++ src/cli/shortener-cli.ts
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+// Libraries
+import { nanoid } from 'nanoid';
+import * as commander from 'commander';
+import * as chalk from 'chalk';
+import * as figlet from 'figlet';
+
+// Internal dependencies
+import { Link } from '../models/link';
+import { General } from '../utils/general';
+
+export class ShortenerCLI {
+  private program: commander.Command;
+
+  constructor(program: commander.Command) {
+    this.program = program;
+  }
+
+  /**
+   * Initializes the CLI program
+   *
+   */
+  public async init() {
+    this.program
+      .version('0.0.1')
+      .description('CLI to shorten links and fetch statistics')
+      .option('--shorten <url>', 'Shortens an url')
+      .option('--stats <url>', 'Shows statistics for a shortened link')
+      .option('--disable <url>', 'Disables a shortened url')
+      .option('--enable <url>', 'Enables a previously disabled shortened url')
+      .parse(process.argv);
+
+    const options = this.program.opts();
+
+    if (options.shorten) {
+      await this.shorten(options.shorten);
+    } else if (Object.keys(options).length < 1) {
+      console.log(
+        chalk.red(figlet.textSync('skillshare', { horizontalLayout: 'full' }))
+      );
+
+      this.program.help();
+    }
+
+    process.exit(0);
+  }
+
+  /**
+   * Shortens the given URL
+   *
+   * @param { string } url
+   *
+   * @return { Promise<void> }
+   */
+  private async shorten(url: string): Promise<void> {
+    if (!General.isValidURL(url)) {
+      console.log(
+        'Please provide a valid URL as an argument. Example: sk --shorten https://google.com'
+      );
+
+      process.exit(0);
+    }
+
+    const link = new Link();
+
+    link.target = url;
+    link.slug = nanoid(10);
+
+    try {
+      await link.save();
+    } catch (error) {
+      console.error('An error occured while saving the link', error);
+    }
+
+    console.log(
+      `Link has been shortened! You can open it here: ${process.env.BASE_URL}/${link.slug}`
+    );
+  }
+}
---
+++ src/cli/sk.ts
@@ -0,0 +1,19 @@
+import { createConnection, getConnectionOptions } from 'typeorm';
+import { Command } from 'commander';
+import { ShortenerCLI } from './shortener-cli';
+import * as path from 'path';
+
+getConnectionOptions()
+  .then((options) => {
+    return createConnection({
+      ...options,
+      entities: [path.join(__dirname, '..', 'models/**/*.js')],
+    });
+  })
+  .then((connection) => {
+    const program = new Command();
+    const cli = new ShortenerCLI(program);
+
+    cli.init();
+  })
+  .catch((error) => console.error(error));
